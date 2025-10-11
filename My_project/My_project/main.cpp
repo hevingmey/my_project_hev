483a421071be788ff8b082dcb681790c1fec9933
hevingmey <grisenkov265@gmail.com>
2025-10-12 00:33:25 +0100

    Inistal commit

---
+++ My_project/My_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  My_project
+//
+//  Created by hevingmey on 12/10/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
